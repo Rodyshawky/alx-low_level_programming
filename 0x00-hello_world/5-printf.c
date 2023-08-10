b79e48a606e2620479c2a3ecd6d96ad453269ffa
Rodyshawky <rodashawkei@gmail.com>
2023-08-10 12:03:35 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main Entry point
+ *
+ * Description: A C program use printf function
+ *
+ * Return: Always 0 (succses)
+*/
+  int main(void)
+  { 
+     printf("with proper grammar, but the outcome is a piece of art,\n");
+     return (0);
+  }
+
