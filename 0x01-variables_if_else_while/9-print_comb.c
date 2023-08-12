e3947a696b4ec7e9aaede060407fb39474f87008
Rodyshawky <rodashawkei@gmail.com>
2023-08-11 17:20:29 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,12 @@ int main(void)
 int a = 0;
 while (a <= 9)
 {
-putchar(a + '0');
-if (a == 9)
-putchar('$');
-else
+putchar(a + 48);
+if (a != 9)
 putchar(',');
 putchar(' ');
 a++;
 }
+putchar('\n');
 return (0);
 }
