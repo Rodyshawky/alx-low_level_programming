6f40d448c130081f34bbb751549c24e91016e243
Rodyshawky <rodashawkei@gmail.com>
2023-08-15 07:02:26 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - print the lowercase characters ten times
+ *
+ * Return: No value retun
+*/
+
+void print_alphabet_x10(void)
+{
+
+char ch = 'a';
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+while (ch <= 'z')
+{
+_putchar('ch');
+ch++;
+}
+_putchar('\n');
+}
+}
