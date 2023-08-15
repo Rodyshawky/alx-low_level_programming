28af879912220e2051e668e22f732dbe8fe3652f
Rodyshawky <rodashawkei@gmail.com>
2023-08-15 11:32:58 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * add -add two numbers
+ * @a: first input number
+ * @b: second input number
+ * Return: On success 0.
+ */
+
+int add(int a, int b)
+{
+int sum = 0;
+sum = a + b;
+return (sum);
+}
+
+
