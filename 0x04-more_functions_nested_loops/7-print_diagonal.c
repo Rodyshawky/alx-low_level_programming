43af6d07d200b259f5ec957f509d7e62a5219bc4
Rodyshawky <rodashawkei@gmail.com>
2023-08-17 14:36:16 -0700

    commit 6

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_diagonal(int n)
+{
+{
+int line, p;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (line = 1; line <= n; line++)
+{
+for (p = 1; p <= line; p++)
+_putchar(' ');
+_putchar(92);
+_putchar('\n');
+}
+}
+}
