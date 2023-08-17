76e618ff22fecabd664de43d4783c85328f7f75d
Rodyshawky <rodashawkei@gmail.com>
2023-08-17 04:42:53 -0700

    it is a commit 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,15 @@
 void more_numbers(void)
 {
 int i, num;
-
 for (i = 0; i < 10; i++)
 {
 for (num = 0; num < 15; num++)
 {
+	if (num > 9)
+	{
+		_putchar(1 + 48);
+		num = num % 10;
+	}
 	_putchar(num + 48);
 }
 }
