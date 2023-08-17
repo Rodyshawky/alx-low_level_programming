a4485420effca6318f23c4536a8e14cc9ac4e214
Rodyshawky <rodashawkei@gmail.com>
2023-08-17 14:40:10 -0700

    commit 6

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,6 @@
 
 void print_diagonal(int n)
 {
-{
 int line, p;
 
 if (n <= 0)
@@ -18,13 +17,11 @@ if (n <= 0)
 _putchar('\n');
 }
 else
-{
 for (line = 1; line <= n; line++)
 {
 for (p = 1; p <= line; p++)
-_putchar(' ');
+       _putchar(' ');
 _putchar(92);
 _putchar('\n');
-}
 }
 }
