d24360eb72e91f89ef2b4b6efd146e15f489fa11
Rodyshawky <rodashawkei@gmail.com>
2023-08-17 13:21:10 -0700

    it is commit 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers -prints the numbers from 0 to 9 except 2 ,4
- * Return: not return any value.
+ * Return: return0 sucess.
  */
 
 void print_most_numbers(void)
@@ -11,8 +11,9 @@ int n = 0;
 
 while (n >= 0 && n <= 9)
 {
-if (n != 2 || n != 4)
+if (n == 2 || n == 4)
 {
+continue;
 _putchar(n + 48);
 }
 n++;
