87dc8e6575ba4fede2fad2ac01e6cc7ed2c17ff9
Rodyshawky <rodashawkei@gmail.com>
2023-08-18 09:32:16 -0700

    it is a new commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,11 +13,15 @@ void print_line(int n)
 int line;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
-
+	{
 		for (line = 1; line <= n; line++)
-			_putchar('_');
-		_putchar('\n');
-
+		{
+			_putchar(92);
+		}
+	_putchar('\n');
+	}
 }
