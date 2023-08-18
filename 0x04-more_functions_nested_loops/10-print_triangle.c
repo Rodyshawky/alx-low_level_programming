dbd74f58952bfaadc967890c77354bffae97e5e6
Rodyshawky <rodashawkei@gmail.com>
2023-08-17 17:39:20 -0700

    its a test commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * print_triangle- print_triangle using #
+ *
+ * @size: input function
+ *
+ * Return: Always 0 (sucsses)
+ */
+
+void print_square(int size)
+{
+int i, j;
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (i = 1; i <= size ; i++)
+{
+	for (j = 1; j <= size; j++)
+		if ((i + j) <= size)
+			_putchar(' ');
+		else
+			_putchar('#');
+}
+		_putchar('\n');
+}
