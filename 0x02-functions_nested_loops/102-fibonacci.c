b1ee8565238470a144226bf2b13abf8721dbdd41
Rodyshawky <rodashawkei@gmail.com>
2023-08-19 11:58:19 -0700

    advanced

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 1;
+	int x = 1;
+	int i, swap;
+	printf("%d",n);
+	for (i = 1; i <= 50; i++)
+	{
+	swap = x;
+	x = n;
+	n = swap + x;
+	printf("%d",n);
+	}
+return (0);
+}
