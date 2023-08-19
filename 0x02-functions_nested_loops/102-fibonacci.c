9e136f461b64b3f01e37ed6f67acbd3f48e3865b
Rodyshawky <rodashawkei@gmail.com>
2023-08-19 12:00:34 -0700

    advanced

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,13 +10,14 @@ int main(void)
 	int n = 1;
 	int x = 1;
 	int i, swap;
-	printf("%d",n);
+
+	printf("%d", n);
 	for (i = 1; i <= 50; i++)
 	{
 	swap = x;
 	x = n;
 	n = swap + x;
-	printf("%d",n);
+	printf("%d", n);
 	}
 return (0);
 }
