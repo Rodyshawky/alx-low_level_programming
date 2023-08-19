90018a5a1ef91f932ac27692723737a2abbaf736
Rodyshawky <rodashawkei@gmail.com>
2023-08-19 11:03:25 -0700

    adv

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,10 +11,10 @@ void print_times_table(int n)
 {
 int i, j;
 
-for (i = 0; i < n ; i++)
+for (i = 0; i <= n ; i++)
 {
 	printf("0, ");
-	for (j = 0; j < n ; j++)
+	for (j = 0; j <= n ; j++)
 	{
 	if (n < 0 || n > 15)
         	break;
