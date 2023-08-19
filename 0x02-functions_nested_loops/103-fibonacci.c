eea21047008ceb3e8b88e2b230fe7b0ac69f0c9a
Rodyshawky <rodashawkei@gmail.com>
2023-08-19 12:41:44 -0700

    advanced

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 1;
+int x = 1;
+int i, swap;
+double sum = 0;
+
+while (sum < 4000000)
+{
+swap = x;
+x = n;
+n = swap + x;
+if (n % 2 == 0)
+{
+sum += n;
+}
+printf("%f", sum);
+}
+printf("\n");
+return (0);
+}
