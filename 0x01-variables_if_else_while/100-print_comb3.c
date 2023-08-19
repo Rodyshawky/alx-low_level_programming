af9a96ccf1cbcdcd0ed3ef04f0a3a32f4054f7ca
Rodyshawky <rodashawkei@gmail.com>
2023-08-19 09:39:36 -0700

    advanced task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,10 +13,12 @@ for (j = 0; j < 10 ; j++)
 {
 for (i = 0; i < 10 ; i++)
 {
-putchar(j + 48);
-putchar(i + 48);
-putchar(',');
-putchar(' ');
+	if (j != i && i == 0)
+		continue;
+	putchar(j + 48);
+	putchar(i + 48);
+	putchar(',');
+	putchar(' ');
 }
 putchar('\n');
 }
