f6e5c62f600c044d42d3023f575fbc3e3ea1dcd7
Rodyshawky <rodashawkei@gmail.com>
2023-08-19 09:17:10 -0700

    advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,18 @@
 
 int main(void)
 {
-int i;
+int i, j;
 
+for (j = 0; j < 10 ; j++)
+{
 for (i = 0; i < 10 ; i++)
 {
-putchar(48);
+putchar(j + 48);
 putchar(i + 48);
 putchar(',');
 putchar(' ');
 }
 putchar('\n');
+}
 return (0);
 }
