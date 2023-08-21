03c94db4e303e250d2f1961b85d3e77511eb136d
Rodyshawky <rodashawkei@gmail.com>
2023-08-21 05:12:46 -0700

    pointer task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * _puts - prints a string
+ * @str: input pointer
+ * Return: On success 0.
+ */
+
+void _puts(char *str)
+{
+for (; *s != '\0'; s++)
+	_putchar(*s + 48);
+}
