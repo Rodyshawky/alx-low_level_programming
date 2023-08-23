34503e3147ee94528372a91889207a6e6a61228b
Rodyshawky <rodashawkei@gmail.com>
2023-08-23 08:31:29 -0700

    more pointer task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: input pointer
+ * @src: input string
+ * Return: pointer of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i, len, size;
+int n = 0;
+
+while (*dest != '\0')
+len++;
+while (*src != '\0')
+size++;
+	for (i = len - 1; i < len + size + 1; i++)
+	{
+		dest[i] == src[n];
+		n++;
+	}
+	dest[len + size] = '\0';
+}
