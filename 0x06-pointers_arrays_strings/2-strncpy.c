a44af65cde38f44c44e37b948b06ad83d1688cbd
Rodyshawky <rodashawkei@gmail.com>
2023-08-23 11:05:02 -0700

    more pointer task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _strncpy - copy string
+ * @dest: input pointer
+ * @src: input string
+ * @n: input number
+ * Return: pointer of dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i <= n; i++)
+dest[i] = src[i];
+dest [n + 1] = '\0';
+return (dest);
+}
