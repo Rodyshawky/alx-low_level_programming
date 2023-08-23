7e1bbf278f36bdffe40f5da28ea3c16b830c42c5
Rodyshawky <rodashawkei@gmail.com>
2023-08-23 10:25:30 -0700

    more pointer task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,13 +14,12 @@ char *_strncat(char *dest, char *src, int n)
 int destlen = 0;
 int srclen = 0;
 int i;
-int d = sizeof(dest);
-int s = sizeof (src);
+
 for (i = 0; dest[i] != '\0'; i++)
 destlen++;
 for (i = 0; src[i] != '\0'; i++)
 srclen++;
-for (i = 0; s > 0; i++)
+for (i = 0; n && src[i] != '\0'; i++)
 dest[destlen + i] = src[i];
 return (dest);
 }
