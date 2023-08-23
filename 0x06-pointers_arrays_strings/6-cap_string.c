d9fa94ba4107a4f2479e3c93512d2bb32cacc5f8
Rodyshawky <rodashawkei@gmail.com>
2023-08-23 14:53:01 -0700

    more pointer task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,7 @@
 /**
  * cap_string -  capitalize letters
  * @a: input pointer
- * Return : always 0 if success
+ * Return: pointer of an array
  */
 
 char *cap_string(char *a)
@@ -14,8 +14,20 @@ for (i = 0; a[i] != '\0'; i++)
 {
 while (!(a[i] >= 'a' && a[i] <= 'z'))
 i++;
-if (a[i - 1] == ' ' || a[i - 1] == '.' || a[i - 1] == ';' || a[i] == ',' || a[i - 1] == '!' || a[i - 1] == '\t' || a[i - 1] == '\n'
-|| a[i - 1] == '?' || a[i - 1] == '(' || a[i - 1] == ')' || a[i - 1] == '{' || a[i - 1] == '}' || a[i - 1] == '"' || i == 0)
+if (a[i - 1] == ' ' ||
+a[i - 1] == '.' ||
+a[i - 1] == ';' ||
+a[i] == ',' ||
+a[i - 1] == '!' ||
+a[i - 1] == '\t' ||
+a[i - 1] == '\n'
+|| a[i - 1] == '?' ||
+a[i - 1] == '(' ||
+a[i - 1] == ')' ||
+a[i - 1] == '{' ||
+a[i - 1] == '}' ||
+a[i - 1] == '"' ||
+i == 0)
 a[i] = a[i] - 32;
 }
 return (a);
