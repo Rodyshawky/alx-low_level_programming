2d6d8d641e13fa20e95fd06c43819a117e847bd4
Rodyshawky <rodashawkei@gmail.com>
2023-08-23 09:12:57 -0700

    more pointer task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,8 +12,10 @@ char *_strcat(char *dest, char *src)
 {
 int i, len;
 
-while (*dest != '\0')
+for (i = 0; dest[i] != '\0'; i++)
+{
 len++;
+}
 	for (i = 0 ; src[i] != '\0' ; i++)
 	{
 		dest[len] = src[i];
