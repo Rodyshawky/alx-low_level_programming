3e9940e8565bc6f072da4fe3ae077ee1916ea931
Rodyshawky <rodashawkei@gmail.com>
2023-08-28 11:11:56 -0700

    even more pointer task

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,17 +8,17 @@
  * Return: pointer s.
  */
 
-char * _strpbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
 int i, j;
 
 for (i = 0; s[i] != '\0'; i++)
 {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-                if (s[i] == accept[j])
-               		return (s + i);
-        }
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+return (s + i);
+}
 }
 return (0);
 }
