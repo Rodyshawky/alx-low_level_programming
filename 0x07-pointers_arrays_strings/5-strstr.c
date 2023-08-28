4fd8f9b75f20f596fabb9dd786b5e585d7bbf316
Rodyshawky <rodashawkei@gmail.com>
2023-08-28 15:03:19 -0700

    even more pointer task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,7 +17,7 @@ for (i = 0; haystack[i] != '\0'; i++)
 for (j = 0; needle[j] != '\0'; j++)
 {
 if (haystack[i] == needle[j])
-return (haystack);
+return (haystack + i);
 }
 if (needle[j] == '\0')
 return (needle);
