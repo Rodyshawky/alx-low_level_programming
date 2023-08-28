3b3f2c744e4bfcf94cb4429b2b353a37e802a595
Rodyshawky <rodashawkei@gmail.com>
2023-08-28 05:42:32 -0700

    Even more pointer task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: input pointer string
+ * @b: input char
+ * @n: input number
+ * Return: pointer s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n && *s != '\0'; i++)
+{
+s[i] = b;
+}
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Tjis is my Readme File
