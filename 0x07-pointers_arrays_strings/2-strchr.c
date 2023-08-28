8f2afcaaecdfbc9262c7d52450067db155f5eea3
Rodyshawky <rodashawkei@gmail.com>
2023-08-28 07:32:00 -0700

    even more pointer task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: input pointer string
+ * @c: input char
+ * Return: pointer s.
+ */
+
+char *_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if ( s[i] == c)
+return (s + i);
+}
+return (NULL);
+}
