1c668f7900b35bdf2438b6e9486435e1dceb4643
Rodyshawky <rodashawkei@gmail.com>
2023-08-28 06:38:26 -0700

    Even more pointer task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _memcpy -copies memory area
+ * @dest: pointer input
+ * @src: pointer input
+ * @n: input number 
+ * Return: pointer dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n && n > 0 && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
