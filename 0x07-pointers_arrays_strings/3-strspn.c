fe90ba67fa516f9b9afef5d62004848e847cb17b
Rodyshawky <rodashawkei@gmail.com>
2023-08-28 08:23:25 -0700

    even more pointer task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,12 +10,15 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-int i;
+int i, j;
 
-for (i = 0; s[i] >= '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-if (s[i] == *accept)
-return (i + 1);
+	for (j = 0; s[i] != accept[j] ; j++)
+	{
+		if (accept[j] == '\0')
+			return (i);
+	}
 }
 return (0);
 }
