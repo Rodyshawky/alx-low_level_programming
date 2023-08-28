39ef604a6de623eeaaf2798d8ab991a10f6b370d
Rodyshawky <rodashawkei@gmail.com>
2023-08-28 14:55:20 -0700

    even more pointer task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+
+/**
+ * _strstr - locates a substring
+ * @haystack: input pointer
+ * @needle: input pointer
+ * Return: pointer string.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+
+for (i = 0; haystack[i] != '\0'; i++)
+{
+        for (j = 0; needle[j] != '\0'; j++)
+        {
+		if (haystack[i] == needle[j])
+               return (haystack);
+	}
+	if (needle[j] == '\0')
+		return (needle);
+}
+return (NULL);
+}
