90aa9881bb450f3eb6ce588f58ff9365d5776ac0
Rodyshawky <rodashawkei@gmail.com>
2023-09-01 05:50:59 -0700

    0x0A Task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,6 +12,6 @@ int main(int argc, char *argv[])
 if (argc < 2)
 printf("Error\n");
 else
-printf("%d\n", atoi(argv[1]) * atoi(argv [2]));
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
