100dd88a8e4ffc7404a1b18052c1f3e2cfb168c1
Rodyshawky <rodashawkei@gmail.com>
2023-09-01 05:12:23 -0700

    0x0A Task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *@argc - count of command line
+ @argv - pointer array string
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
 (void) argc;
-printf("%s\n",argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
