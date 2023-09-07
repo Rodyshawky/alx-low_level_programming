f46ed7a7d3d88b7736e51fb10d285c85aa8e7844
Rodyshawky <rodashawkei@gmail.com>
2023-09-07 16:20:21 -0700

    0x0c task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,11 +19,11 @@ if (s1 == NULL)
 	s1 = "";
 if (s2 == NULL)
 	s2 = "";
-for (i = 0; s1[i] != '\0'; i++)
+for (i = 0; s1[i]; i++)
 {
 	len1++;
 }
-for (i = 0; s2[i] != '\0'; i++)
+for (i = 0; s2[i]; i++)
 {
 	len2++;
 }
