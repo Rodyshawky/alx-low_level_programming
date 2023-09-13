cdf9cf110e953c9e959c4dfd8b97f7490ae94b8f
Rodyshawky <rodashawkei@gmail.com>
2023-09-13 08:12:16 -0700

    0x0f task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,10 +16,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	if (array == NULL)
 		return (-1);
+	if (cmp == NULL)
+		return (-1);
 	for (i = 0; i < size; i++)
+	{
 		if (cmp(array[i]))
 			return (i);
-		else
-			return (-1);
-return (0);
+	}
+	return (-1);
 }
