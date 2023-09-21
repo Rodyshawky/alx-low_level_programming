47e59d056c60689ef650ae2b64ab7431bad54f11
rodyshawky <rodashawkei@gmail.com>
2023-09-21 13:35:02 -0700

    0x12 task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t num_nodes = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
@@ -20,9 +20,10 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		num_nodes++;
+		nodes++;
 		h = h->next;
 	}
 
-	return (num_nodes);
+	return (nodes);
+
 }
