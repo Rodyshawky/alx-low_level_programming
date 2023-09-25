c8da53a661d4004c50928d2a2b68fbd8941fbab2
rodyshawky <rodashawkei@gmail.com>
2023-09-25 02:29:40 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+#include<stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * print_listint- print all element on the list
+ * @h: pointer to struct
+ * Return: count of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
