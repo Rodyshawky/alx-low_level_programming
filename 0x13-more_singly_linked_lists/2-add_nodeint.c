ae9209525f0e681cd7e2639b78f55a6154cf005c
rodyshawky <rodashawkei@gmail.com>
2023-09-25 08:32:36 -0700

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,25 @@
 #include "lists.h"
+#include<stdlib.h>
 /**
- * print_listint - a function that prints all the elements of a listint_t list.
- * @h: struct
- * Return: number of nodes
+ * add_nodeint - a function that add node
+ * @head: struct
+ * @n: number
+ * Return: pointer to address of new node or NULL if it failed
  */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *h;
 
+	h = malloc(sizeof(listint_t));
+	if (h == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+	h->n = n;
+	h->next = *h;
+	*h->next;
+	}
+	return (h);
+}
