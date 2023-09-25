ac117f1739374bc390f4a835242b31c53c2a6c61
rodyshawky <rodashawkei@gmail.com>
2023-09-25 12:47:15 -0700

    Task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,25 @@
+#include"lists.h"
+/**
+ * insert_nodeint_at_index - inserts a listint_t node at index in list
+ *
+ * @head: head of list
+ * @idx: index to add node at, starting at 0
+ * @n: value of node to add
+ *
+ * Return: new node address if success, NULL otherwise
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *h;
+
+	h = *head;
+	while (i <= idx)
+	{
+		h = h->next;
+		i++;
+	}
+	h->n = n;
+	h = h->next;
+	return (h);
+}
