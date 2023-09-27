60f0f80ef92e7f5a59c5217fc9ad680da9c9f6f0
rodyshawky <rodashawkei@gmail.com>
2023-09-27 11:23:49 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,4 +33,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp->next = h->next;
 	h->next = tmp;
 	return (tmp);
+	if (idx > i)
+                return (NULL);
 }
