ec8e5350b6c55d37174166ed8552365db0f111af
rodyshawky <rodashawkei@gmail.com>
2023-09-27 10:55:12 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	h = *head;
 	tmp->n = n;
 	tmp->next = NULL;
-	if (*head != NULL || idx == 0)
+	if (*head != NULL && idx == 0)
 	{
 		tmp->next = *head;
 		*head = tmp;
