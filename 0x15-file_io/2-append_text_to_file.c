449c399a83fbeb07e4359161b4df557d5c4a3c83
rodyshawky <rodashawkei@gmail.com>
2023-10-02 09:27:16 -0700

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,9 +19,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	fd = open(filename, O_WRONLY, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	write(fd, text_content, strlen(text_content));
+	close(fd);
 	return (1);
 }
