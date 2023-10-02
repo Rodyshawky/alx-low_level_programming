848df69f324ce03f244dbde4787b5fb307c94c1b
rodyshawky <rodashawkei@gmail.com>
2023-10-02 08:57:28 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	}
 	len = strlen(text_content);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNK , 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC , 0600);
 	if (fd == -1)
 		return (-1);
 	write(fd, text_content, len);
