67bd93ef6ab084774ca1c304e0967428ab61d832
rodyshawky <rodashawkei@gmail.com>
2023-12-22 15:10:02 -0800

    task0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+*hash_table_create - create hash table
+*@size: size of hash table
+*Return: new hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+	unsigned long int i;
+
+	new_table = malloc(sizeof(hash_table_t));
+	if (!new_table)
+		return (NULL);
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			new_table->array[i] = NULL;
+		}
+	}
+	return (new_table);
+}
